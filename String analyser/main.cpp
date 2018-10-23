4938e367ef06eb126934cda3668fa5da9818a0db
William MacLeod <wmac0002@student.monash.edu>
2018-10-23 13:51:49 +1100

    refactor into different file

--- String analyser/main.cpp
+++ String analyser/main.cpp
@@ -7,52 +7,7 @@
 #include <string>
 using namespace std;
 
-string name;
-char characterToFind;
-
-void analyse() {
-	cout << "String analyser program" << endl;
-	cout << "------------------------" << endl;
-
-	cout << "please enter a name: ";
-	cin >> name;
-
-	cout << "string to analyse is : " << name << endl;
-	cout << "-------------------------" << endl;
-	cout << "           INFO          " << endl;
-	cout << "-------------------------" << endl;
-
-	cout << name << " uses " << name.size() << " bites " << endl;
-	cout << name << " has " << name.length() << " letters " << endl;
-	cout << "The first letter of the name is: " << name.front() << endl;
-	cout << "The last letter of the name is: " << name.back() << endl;
-
-	//finding character index
-	cout << "-------------------------" << endl;
-	cout << "      INDEX FINDER       " << endl;
-	cout << "-------------------------" << endl;
-
-	cout << "enter a character to find its index: " << characterToFind;
-	cin >> characterToFind;
-	cout << characterToFind << " is the letter you chose!" << endl;
-
-	cout << "---------USING string.find() APPROACH----------" << endl;
-	cout << "the character you chose is at index " << name.find(characterToFind) << endl;
-
-	cout << "-----------FORLOOP/CONDITIONAL APPROACH-----------" << endl;
-	//if the index of the character in the name string does not have a position, it will exit.
-	if (name.find(characterToFind) == name.npos) {
-		cout << "doesnt exist" << endl;
-	}
-	else {
-		for (int j = 0; j < name.length(); j++) {
-			if (name[j] == characterToFind) {
-				cout << "The letter " << name[j] << " is at index: " << j << endl;
-			}
-		}
-	}
-	system("pause");
-}
+void analyse();
 
 int main() {
 	analyse();
